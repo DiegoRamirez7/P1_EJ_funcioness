0955e5f4bc5abfdd234b858ac2168eadad415b29
DiegoRamirez7 <a.22308051281091@cbtis128.edu.mx>
2025-02-07 16:48:18 +0000

    otra vez

--- funcion1.dart
+++ funcion1.dart
@@ -10,6 +10,7 @@ print("diego ramirez");
   void main(){
   //diego ramirez dominguez a.22308051281091return
   //llamar a la funcion saludar
+  
   print("diego ramirez dominguez mat:22308051281091");
   print("llamando a la funcion saludar");
   saludar();
