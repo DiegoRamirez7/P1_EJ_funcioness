312a673481eb09cc9d99ea6f28bbab231486be30
DiegoRamirez7 <a.22308051281091@cbtis128.edu.mx>
2025-02-17 18:23:04 +0000

    clase1

--- clase1.dart
+++ clase1.dart
@@ -1,19 +1,34 @@
 class Persona {
-  // Atributos (propiedades)
   String nombre;
   int edad;
 
   // Constructor
   Persona(this.nombre, this.edad);
 
-  // Función para cumplir años
-  void cumplirAnios() {
-    edad++; // Incrementa la edad en 1
-    print('¡Feliz cumpleaños, $nombre! Ahora tienes $edad años.');
+  // Método para saludar
+  void saludar() {
+    print("Hola soy $nombre");
   }
 
-  // Función para mostrar información
-  void mostrarInformacion() {
-    print('Nombre: $nombre, Edad: $edad años');
+  // Método para cumplir años
+  void cumpliranos() {
+    edad++;
+    print("Feliz cumpleaños $nombre! Ahora tienes $edad años.");
   }
 }
+
+void main() {
+  print("diego ramirez dominguez mat:22308051281091");
+
+  // Crear una instancia de Persona
+  Persona persona = Persona("Diego", 20);
+
+  // Acceder a los atributos
+  print("nombre: ${persona.nombre}");
+  print("edad: ${persona.edad}");
+
+  // Llamar a los métodos
+  persona.saludar();
+  persona.cumpliranos(); // Aquí llama al nuevo método para incrementar la edad
+}
+
